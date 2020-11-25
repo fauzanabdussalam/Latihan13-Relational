cfab3a4c99e3d20d7bfeaa4aa8fe12db7dbacbd7
fauzan_abdussalam <fauzan@3trust.com>
2020-11-25 23:09:26 +0700

    Initial Commit

---
+++ src/pbo10k/pkg10119901/latihan13/relational/PBO10K10119901Latihan13Relational.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo10k.pkg10119901.latihan13.relational;
+
+/**
+ *
+ * @author
+ * NAMA     : Fauzan Muhammad Abdussalam
+ * KELAS    : IF-10K
+ * NIM      : 10119901
+ * Deskripsi Program : Menampilkan Program Relational
+ *
+ */
+public class PBO10K10119901Latihan13Relational {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int bil1 = 10;
+        int bil2 = 20;
+        
+        System.out.println("a == b = " + (bil1 == bil2));
+        System.out.println("a != b = " + (bil1 != bil2));
+        System.out.println("a > b = " + (bil1 > bil2));
+        System.out.println("a < b = " + (bil1 < bil2));
+        System.out.println("b >= a = " + (bil2 >= bil1));
+        System.out.println("b <= a = " + (bil2 <= bil1));
+    }
+    
+}
